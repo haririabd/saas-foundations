30484fba6681d398a8f8262a02b4fc696f26ea89
haririabd <hariri.bin.abdullah@gmail.com>
2025-01-03 23:54:47 +0800

    added model and view to display and save data in db

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,19 +1,22 @@
+import pathlib
 from django.shortcuts import render
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def index_view(request, *args, **kwargs):
     page_title = "Homepage"
-    html_template = "index.html",
+    html_template = "index.html"
+    qs = PageVisit.objects.all().count()
+    q = PageVisit.objects.filter(path=request.path).count()
 
     context = {
-        "page_title": page_title
+        "page_title": page_title,
+        "total_visit": qs,
+        "page_visit": q
     }
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, context)
 
 def about_view(request, *args, **kwargs):
-    page_title = "About"
-    html_template = "index.html",
-    
-    context = {
-        "page_title": page_title
-    }
-    return render(request, html_template, context)
+    return index_view(request, *args, **kwargs)
