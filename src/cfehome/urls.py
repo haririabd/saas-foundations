caa3ac2dba66558e01236556f4059bc506cb095d
haririabd <hariri.bin.abdullah@gmail.com>
2025-01-03 22:24:10 +0800

    simple views and templating

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,4 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_view),
+    path('about/', views.about_view),
 ]
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 def index_view(request, *args, **kwargs):
-    template_name = 'index.html',
-    context = {}
-    return render(request, template_name, context)
+    page_title = "Homepage"
+    html_template = "index.html",
+
+    context = {
+        "page_title": page_title
+    }
+    return render(request, html_template, context)
+
+def about_view(request, *args, **kwargs):
+    page_title = "About"
+    html_template = "index.html",
+    
+    context = {
+        "page_title": page_title
+    }
+    return render(request, html_template, context)
