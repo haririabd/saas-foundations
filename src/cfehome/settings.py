62162c0f450c3cd0dc258a0a373c5600a675db4b
haririabd <hariri.bin.abdullah@gmail.com>
2025-01-03 21:15:04 +0800

    edit to show host

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +33,8 @@ if DEBUG:
         '127.0.0.1',
         'localhost',
     ]
+
+print("Host: ", ALLOWED_HOSTS)
 
 # Application definition
 
